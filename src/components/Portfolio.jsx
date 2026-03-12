f6c503443e05233a985a4a07b70a1571e952fedb
kavitha-047 <kavithakanagaraj4@gmail.com>
2026-03-12 22:06:00 +0530

    updated tags

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -30,7 +30,7 @@ const Portfolio = () => {
             title: "Client Portfolio",
             description: "A modern and responsive portfolio website created for my client,to showcase profile, skills, and personal brand with a clean and aesthetic design.",
             image: "https://images.unsplash.com/photo-1557821552-17105176677c?q=80&w=2064&auto=format&fit=crop",
-            tags: ["tailwind", "Stripe", "wordpress"],
+            tags: ["tailwind", "React", "Framer Motion"],
             live: "https://manjulasivasamy.netlify.app/",
             github: "https://github.com/kavitha-047/manju--portfolio"
         },
